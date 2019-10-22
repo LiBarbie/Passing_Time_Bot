16b0f0212e257a33ea764a3292a58f13db5efb55
Linda Barbiero <linda.barbiero.st@everis.com>
2019-10-22 12:57:26 +0100

    Added the function to read and remove a selected page based on its date; Added some comments to explain the code

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ const fs = require("fs");
 const fs_dir = "./log";
 let fs_path = fs_dir+"/diary";
 
+//array of "drink" intents
 const drinks = {
     "water" : "https://i.pinimg.com/originals/a2/c7/ba/a2c7bad51bf0fb1ccdcada8916d08774.gif",
     "coffee": "http://supacoffee.com/wp-content/uploads/2017/11/Supa_Coffee_Anim.gif",
@@ -20,10 +21,12 @@ const drinks = {
     "martini" : "https://media2.giphy.com/media/3JPdkGGWUMWANyrlTy/source.gif"
 };
 
+//array of "salutation" intents
 const salutation =  [
     "hello","greetings","salutations","good evening","good morning","aloha","hi","hola","sup","good afternoon","good day","hey","howdy","heya","bonjour"
 ];
 
+//array of "thank" intents
 const thanks = [
     "thankyou","thanks","thank you", "I appreciated that","You're kind"
 ];
@@ -33,10 +36,19 @@ class MyBot extends ActivityHandler {
         super();
         //Gets the current date
         const date = this.get_this_date();
-        fs_path = fs_path+date;
 
-        //Writes - 0 : Not in writing or removing mode; 1 : The user's writing; 2 : The user wants to remove his page.
+        /*Writes - 
+        0 : The user doesn't want to interact with the diary (writing, reading, removing, ...)
+        1 : The user's writing.
+        2 : The user wants to read the selected page.
+        3 : The user wants to remove a page.
+        4 : The user agreed to remove the selected page.
+        */
         let writes = 0;
+
+        //It's for the removing mode.
+        let sel_date="";
+
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
             //Context Flag - 0 : Context not found (or not found yet); 1 : Context found.
@@ -46,7 +58,8 @@ class MyBot extends ActivityHandler {
             //Writing, Reading and Removing Session - Level 1
             //The user is currently writing on the log file.
             if(writes==1 && cntxt==0){
-                fs.appendFile(fs_path, mex+"\n\n", function(err) {
+                const fs_path_write = fs_path+date;
+                fs.appendFile(fs_path_write, mex+"\n\n", function(err) {
                     if(err) {
                         return console.log(err);
                     }//if
@@ -56,33 +69,49 @@ class MyBot extends ActivityHandler {
                 cntxt=1;
             }//if-writes
 
-            //The user wants to write in the log file.
+            //The user wants to write in the diary. They wrote the keyword "write"
+            //It will automatically be the current date.
             if(mex.includes("write") && writes==0 && cntxt==0){
                 await context.sendActivity("*takes a diary from under the bar corner and lends it to you*\n\n\"Here's your diary, sir. Write everything's up on your mind.\"");
                 writes=1;
                 cntxt=1;
             }//if-wants-write
 
+            //The user wants to read a page of the diary. They wrote the keyword "read"
+            //They have to select a specific date.
             if(mex.includes("read") && writes==0 && cntxt==0){
-                await context.sendPages(context);
+                await this.sendPages(context);
+                writes =2;
                 cntxt=1;
             }//if-wants-read
 
-            //The user wants to let the bot read his file
-            if(mex.includes("read") && writes==0 && cntxt==0){
-                await context.sendActivity("*Takes a diary from under the bar corner and opens it*\n\n\"I'll read your diary, sir, if you please.\"\n\n*clears throat*");
-                fs.readFile(fs_path, 'utf8', function(err, contents) {
+            //The user selected the current date to read.
+            //Now the bot will read it for them if it recognize a date.
+            if(writes==2 && cntxt==0){
+                if(this.ifDate(mex)){
+                    const fs_path_read=fs_path+mex;
+                    await context.sendActivity("*Takes a diary from under the bar corner and opens it*\n\n\"I'll read your diary, sir, if you please.\"\n\n*clears throat*");
+                    fs.readFile(fs_path_read, 'utf8', function(err, contents) {
                     if(contents==undefined) contents="...";
                     context.sendActivity(contents+
-                        "\n\n\"...Hope my interpretation was satisfying\"\n\n*puts the diary under the bar counter*");
-                });//readfile
-                await context.sendActivity("");
+                        "\n\n\"...Hope my interpretation was satisfying\"\n\n");
+                    });//readfile
+                    await context.sendActivity("");
+                }//if-ifDate
+                else{
+                    await context.sendActivity("\"Sir I couldn't understand which date. I'm sorry\"\n\n");
+                }//else
+                await context.sendActivity("*puts the diary under the bar counter*");
                 cntxt=1;
-            }//if-wants-read
+                writes=0;
+            }//if-selecting date
 
-            if(writes==2 && cntxt==0){
+            //The user is in removing mode, selected a date and has to type "yes" if he really wants to remove the selected page.
+            //If the user writes anything else except "yes" the bot will do nothing.
+            if(writes==4 && cntxt==0){
                 if(mex.includes('yes')){
-                    fs.unlink(fs_path, (err) => {
+                    const fs_path_delete = fs_path+sel_date;
+                    fs.unlink(fs_path_delete, (err) => {
                         if (err) {
                         return console.error(err)
                         }
@@ -95,17 +124,33 @@ class MyBot extends ActivityHandler {
                 }//else-anything
                 writes=0;
                 cntxt =1;
-            }
+            }//if-really-removes
 
-            //The user wants to delete his written page of the diary.
+            //The user selected a specific date in the removing mode.
+            //If the bot doesn't recognize a date he won't do anything.
+            if(writes==3 && cntxt==0){
+                if(this.ifDate(mex)){
+                    await context.sendActivity("*takes the diary from under the bar corner and opens it*\n\n\"Are you sure you want me to tear this page apart, sir?\"");
+                    sel_date=mex;
+                    writes=4;
+                }
+                else{
+                    await context.sendActivity("\"Sir I couldn't understand which date. I'm sorry\"\n\n*puts the diary under the bar counter*");
+                    writes=0;
+                }//else
+                cntxt=1;
+            }//if-remove-selected-date
+
+            //The user wants to write in the diary. They wrote the keyword "write"
+            //The user now has to select a date.
             if((mex.includes("remove") || mex.includes("delete")) && writes==0 && cntxt==0){
-                await context.sendActivity("*takes the diary from under the bar corner and opens it*\n\n\"Are you sure you want me to tear this page apart, sir?\"");
-                writes = 2;
-                cntxt = 1;
+                await this.sendPages(context);
+                writes =3;
+                cntxt=1;
             }//if-remove
-            
 
-            //Serving the user with the drink he asked for - Level 2
+            //Serving Drink - Level 2
+            //Serving the user with the drink he asked for
             if(this.getMex(drinks,mex) && writes==0 && cntxt==0){
                 const drink = this.getMex(drinks,mex);
                 await context.sendActivity({ attachments: [this.createAnimationCard(drinks[drink],drink)] });
@@ -114,6 +159,7 @@ class MyBot extends ActivityHandler {
             }//if-asked-for-drink
 
             //Asking for a drink - Level 3
+            //The bot will ask for wich drink the user wants. 
             if(mex.includes("drink")&&writes==0 && cntxt==0){
                 await context.sendActivity("\"Which one, sir?\"");
                 cntxt = 1;
@@ -121,24 +167,28 @@ class MyBot extends ActivityHandler {
 
 
             //Greetings and Thanks - Level 4
+            //The user greeted the bot and the bot will greet them back.    
             if(this.getMex(thanks,mex) && writes==0 && cntxt==0){
                 await context.sendActivity("\"You're welcome.\"");
                 cntxt=1;
             }//if-thanks
 
+            //The user thanked the bot for something
+            //The bot will answer them back.
             if(this.getMex(salutation,mex) && writes==0 && cntxt==0){
                 await context.sendActivity("\"Hello, sir. Can I take your order?\"");
                 cntxt = 1;
             }//if-greetings
 
             //Didn't find any match - Level 0
+            //The bot couldn't find any context for the thing the user asked for.
             if(cntxt == 0 && writes==0){
                 await context.sendActivity("\"We don't do that here. I'm sorry.\"");
             }//if
 
             // By calling next() you ensure that the next BotHandler is run.
             await next();
-        });
+        });//function
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
@@ -150,34 +200,46 @@ class MyBot extends ActivityHandler {
                    reply.attachments = [this.getInlineAttachment()];
                    await context.sendActivity(reply);
                    await context.sendActivity("\"Welcome to the Feels Bar. Please take a seat if you want.\"");
-                }
-            }
+                }//if
+            }//for
             
             // By calling next() you ensure that the next BotHandler is run.
             await next();
-        });
-    }
+        });//function
+    }//onThisMembersAdded
 
+    /*
+    @param array the array of intents (greetings, drinks, thanks, ...)
+    @param mex the current message the user sent.
+    @return the keyworld of the context or a 0 if he didn't find any match with the array of intents.
+    */
     getMex(array,mex){
         var flag = 0;
         var items = Object.keys(array)
         items.forEach(function(item){
             if(mex.includes(array[item]) || mex.includes(item)){
                 flag = item;
-            }
-        });
-
+            }//if
+        });//function
         return flag;
     }//getMex
 
+    /*
+    returns an object in order to display the video to set the mood.
+    */
     getInlineAttachment() {
         return {
             name: 'Welcome to the Feels Bar. Please take a seat if you want.',
             contentType: 'video/mp4',
             contentUrl: 'https://www.youtube.com/watch?v=OGYwcPtDdFA'
-        }
+        }//object
     }//getInlineAttachment
-    
+
+    /*
+        @param urlDrink the url of the drink (gif format)
+        @param drink the word of the drink ("soda","beer",...)
+        @return an animatedcard to display the drink the user asked for.
+    */
     createAnimationCard(urlDrink, drink) {
         return CardFactory.animationCard(
             `-*gives ${drink}*-`,
@@ -188,9 +250,12 @@ class MyBot extends ActivityHandler {
             {
                 subtitle: ''
             }
-        );
-    }
+        );//animationCard
+    }//createAnimationCard
 
+    /*
+        @return the current date in year-month-date format (es. 2000-01-13)
+    */
     get_this_date(){
         let ts = Date.now();
 
@@ -201,8 +266,12 @@ class MyBot extends ActivityHandler {
 
         // prints date & time in YYYY-MM-DD format
         return (year + "-" + month + "-" + date);
-    }//getDate()
+    }//get_this_date()
 
+    /*
+        @param mex the message the user sent.
+        @return the message in Date format or a false if the message isn't a date type.
+    */
     ifDate(mex){
         try{
             const mexDate = new Date(mex).toISOString();
@@ -213,7 +282,10 @@ class MyBot extends ActivityHandler {
         }
     }//ifDate
 
-    
+    /*
+        @param context the context of the bot
+        @return a MessageFactory object wich displays all the dates of the diary pages
+    */
     async sendPages(turnContext) {
         let reply;
         let array=[];
@@ -225,6 +297,6 @@ class MyBot extends ActivityHandler {
         "*takes the diary from under the bar corner and opens it*\n\n\"Which page, sir?\"");
         await turnContext.sendActivity(reply);
     }//sendPages
-}
+}//MyBot
 
 module.exports.MyBot = MyBot;
